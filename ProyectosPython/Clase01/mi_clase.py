7095542864f9ca7ae373171dd7ea18e6843f9abc
Silvana S. Ledesma <113069949+LedesmaSil@users.noreply.github.com>
2023-09-21 18:40:17 -0300

    SEMANA 5 Python
    Profundizando de String

---
+++ ProyectosPython/Clase01/mi_clase.py
@@ -0,0 +1,23 @@
+
+
+class MiClase:
+    '''
+    Este es un ejemplo de la documentación
+    de esta clase.
+    es solo para saber que aquí está MiClase
+    '''
+    def __init__(self):
+        '''
+        Aquí esta el método donde init
+        Este método es el de inicio
+        para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mi que recibe parámetros
+        Detecto los parámetros automaticamente
+        :param param1: Este es el parámetro numero 1
+        :param param2: Este es el parámetro numero 2
+        :return: Este es el valor de retorno
+        '''
