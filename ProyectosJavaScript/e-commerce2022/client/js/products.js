cd0796d9fa43096372107c5a50ee33b6d0cf2fc5
Santiago Martos <SantiMartos30@gmail.com>
2023-11-07 01:11:56 -0300

    Semana 03 JavaScript
    E-Commerce Básico (Parte 3)

--- ProyectosJavaScript/e-commerce2022/client/js/products.js
+++ ProyectosJavaScript/e-commerce2022/client/js/products.js
@@ -3,14 +3,14 @@ const productos = [
         id: 1,
         productName: "GTX 1660 Super",
         price: 209999,
-        quantity: 1,
+        quanty: 1,
         img: "/client/media/gtx1660s.png"
     },
     {
         id:2,
         productName: "RTX 2060 Super",
         price: 274999,
-        quantity: 1,
+        quanty: 1,
         img: "/client/media/rtx2060s.png"
     }
 ]
